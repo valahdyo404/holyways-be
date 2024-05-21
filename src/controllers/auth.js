7267de7991654ebeaae1f74e0cdcd41de8b87791
valahdyo <valahdyo@ilcs.co.id>
2024-05-21 11:01:58 +0700

    check bcrypt

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,7 @@
 const { user } = require("../../models")
 //comment
 const Joi = require("joi")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
 //Login
